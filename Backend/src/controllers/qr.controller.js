07f805d930d357c52c7b2ba0493ddadd4091b292
Yatin_Singh <yatinsingh331@gmail.com>
2025-01-07 00:12:34 +0530

    added get qrs feature

--- Backend/src/controllers/qr.controller.js
+++ Backend/src/controllers/qr.controller.js
@@ -24,5 +24,30 @@ const uploadQr =  asyncHandler(async (req, res)=>{
     }
 })
 
+const getAllQr = asyncHandler(async(req,res)=>{
+    const {page=1, limit=10} = req.query
 
-export {uploadQr}
+    const pageNum = parse(page,10)
+    const limitNum = parse(limit,10)
+    const skipAmt = (pageNum - 1) * limitNum
+
+    try {
+        const allQr = await Qr.find().sort({createdAt:-1}).skip(skipAmt).limit(limitNum)
+    
+        if(!allQr || allQr.length===0){
+            throw new ApiError(400,"No qr found!")
+        }
+        const totalQr =await Qr.countDocuments()
+        
+        const pagination = {
+            totalPages :  Math.ceil(totalQr / limitNum),
+            currentPage : pageNum,
+            totalQr,
+            pageSize: limitNum
+        }
+        return res.status(200).json(new ApiResponse(200,{allQr, pagination}))
+    } catch (error) {
+            console.log("No qr found",error);
+    }
+})
+export {uploadQr, getAllQr}
