16c47705bfcb13eccaadce03dbeac993752d2ca6
Yatin_Singh <yatinsingh331@gmail.com>
2025-01-23 16:22:02 +0530

    added user registration and login functionality

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,59 @@
+import ApiError from "../utils/ApiError.utils";
+import asyncHandler from "../utils/asyncHandler";
+import User from "../models/user.model"
+import ApiResponse from "../utils/ApiResponse.utils";
+
+const generateAccessTokenPlease=asyncHandler(async(userId)=>{
+    const user =await  User.findById(userId)
+    const accessToken = await user.generateAccessToken();
+    
+    return {accessToken}
+})
+const registerUser = asyncHandler(async (req,res)=>{
+    const {username, email, password} = req.body
+    if([username,email,password].some((value)=>(
+        value.trim()===""
+    ))){
+        throw new ApiError(400,"All fields are required!!")
+    }
+
+    const registeredUser = await User.findOne({
+        $or:[{email}, {username}]
+    })
+
+    if(registeredUser){
+        throw new ApiError("User already exists!")
+    }
+    const user = await User.create({
+        username,
+        email,
+        password
+
+    })
+
+    if(!user){
+        throw new ApiError(500,"error while creating the user")
+    }
+    return res.status(200).json(new ApiResponse(200,user,"User registered Successfully!"))
+})
+
+const loginUser = asyncHandler(async(req, res)=>{
+    const {email, password} = req.body
+    if(!email || !password){
+        throw new ApiError(400,"All fields are required")
+    }
+
+    const emailFound = await User.findOne({email})
+    if(!emailFound){
+        throw new ApiError(400,"Email or password is incorrect")
+    }
+    const passwordCheck = await emailFound.isPasswordCorrect(password)
+    if(!passwordCheck){
+        throw new ApiError(400,"Email or password is incorrect")
+    }
+
+    
+
+    return res.status(200).json(new ApiResponse(200,emailFound,"Logged In Successfully!!!"))
+})
+export{registerUser}
