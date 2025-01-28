622aa706cd90ab2d49c2aecb28d6a88f0167136b
Yatin_Singh <yatinsingh331@gmail.com>
2025-01-28 13:21:41 +0530

    setup frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-white  flex'>
+        <div className='w-1/2 bg-blue-700 h-screen' >
+          Yatin Singh is the king
+        </div>
+        <div className='bg-amber-600 w-1/2'>
+          Register For the app.
+        </div>
+      </h1>
+    </>
+  )
+}
+
+export default App
